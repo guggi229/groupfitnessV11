03fe105ee597360a60d0e3591e2205a1f086bb7f
Guggisberg Stefan <guggs2@bfh.ch>
2017-10-11 14:51:10 +0200

    EJB Client Setup funktioniert

--- GroupfitnessV11Tests/src/ch/guggisberg/stefan/test/UserBeanTester.java
+++ GroupfitnessV11Tests/src/ch/guggisberg/stefan/test/UserBeanTester.java
@@ -7,40 +7,33 @@ import java.util.Hashtable;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
-
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
+import ch.guggisberg.stefan.groupfitness.services.LoginBean;
 import ch.guggisberg.stefan.groupfitness.services.UserServiceRemote;
 
 public class UserBeanTester {
-
+	private static Logger log = Logger.getLogger(UserBeanTester.class);
 	private UserServiceRemote usr;
 
 	@Before
 	public void setUp() throws Exception {
-
-
 		try {
 			final Hashtable<String, Comparable> jndiProperties =
 					new Hashtable<>();
 
 			jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
-
 			jndiProperties.put("jboss.naming.client.ejb.context", true);
-
-			jndiProperties.put(Context.PROVIDER_URL, "http-remoting://locahlhost:8080");
+			jndiProperties.put(Context.PROVIDER_URL, "http-remoting://127.0.0.1:8080");
 			jndiProperties.put(Context.SECURITY_PRINCIPAL, "admin");
 			jndiProperties.put(Context.SECURITY_CREDENTIALS, "admin");
-
 			final Context context = new InitialContext(jndiProperties);
-
 			final String lookupName = "GroupfitnessV11-0.0.1-SNAPSHOT/GroupfitnessV11EJB-0.0.1-SNAPSHOT/UserService!ch.guggisberg.stefan.groupfitness.services.UserServiceRemote";
-
 			usr = (UserServiceRemote) context.lookup(lookupName);
-
-
 		} catch (Exception e) {
+			log.info(e);
 			System.out.println(e);
 		}
 
@@ -48,7 +41,7 @@ public class UserBeanTester {
 	}
 	@Test 
 	public void test() {
-		assertNull(usr);
+		assertNotNull(usr);
 	}
 
 
