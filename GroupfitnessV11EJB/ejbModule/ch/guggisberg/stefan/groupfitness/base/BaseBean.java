1eda1eb98ccbf1a50e67cbe03efd78897b938edf
Guggisberg Stefan <guggs2@bfh.ch>
2017-10-16 22:34:18 +0200

    Kursname notNull!

--- GroupfitnessV11EJB/ejbModule/ch/guggisberg/stefan/groupfitness/base/BaseBean.java
+++ GroupfitnessV11EJB/ejbModule/ch/guggisberg/stefan/groupfitness/base/BaseBean.java
@@ -10,25 +10,27 @@ import javax.faces.context.FacesContext;
  * 
  * @author guggi229
  *
- * Generelle Elemente für die App
+ * Generelle Elemente für die App.
+ * 
+ * Lädt das richtige Message Bundle für die Übersetzung
  *
  */
 abstract public class BaseBean implements Serializable {
 
 	private static final long serialVersionUID = 7832409288788294742L;
 
-	public void showGlobalMessage(String message, String key){
-		FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO,message, key);
+	public void showGlobalMessage(String key, String jsfInfo){
+		FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO,getText(key), jsfInfo);
 		FacesContext fc = FacesContext.getCurrentInstance();
 		fc.addMessage(null, facesMsg);
 	}
 
-	public void showGlobalErrorMessage(String message, String key){
-		FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,message, key);
+	public void showGlobalErrorMessage(String key, String jsfInfo){
+		FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,getText(key), jsfInfo);
 		FacesContext fc = FacesContext.getCurrentInstance();
 		fc.addMessage(null, facesMsg);
 	}
-	public String getText(String key) {
+	private String getText(String key) {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		String messageBundleName = facesContext.getApplication().getMessageBundle();
 		Locale locale = facesContext.getViewRoot().getLocale();
