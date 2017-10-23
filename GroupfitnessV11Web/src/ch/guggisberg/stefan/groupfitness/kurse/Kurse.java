907fd8a2da9cf575ef6f66796d18c8fbb35eb840
Guggisberg Stefan <guggs2@bfh.ch>
2017-10-23 12:33:34 +0200

    Korrekter Filename bei Fileupload. Suffix ok!

--- GroupfitnessV11Web/src/ch/guggisberg/stefan/groupfitness/kurse/Kurse.java
+++ GroupfitnessV11Web/src/ch/guggisberg/stefan/groupfitness/kurse/Kurse.java
@@ -1,5 +1,6 @@
 package ch.guggisberg.stefan.groupfitness.kurse;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
@@ -40,7 +41,8 @@ public class Kurse implements Serializable {
 	public void addKurs() throws KursAlreadyExistsException, IOException {
 		kurs = kursService.create(kurs);
 		System.out.println("Kurs ID :" + kurs.getId());
-		file.write("C:\\Users\\guggi229\\Documents\\"+getFilename(file));
+		getFileTyp();
+		file.write("C:\\Users\\guggi229\\Documents\\"+ kurs.getId() + "." + getFileTyp());
 		kurs=null;
 		
 	}
@@ -48,16 +50,20 @@ public class Kurse implements Serializable {
 		return kursService.update(kurs);
 	}
 	
-	private static String getFilename(Part part) {
-        for (String cd : part.getHeader("content-disposition").split(";")) {
-  
-        	if (cd.trim().startsWith("filename")) {
-                String filename = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
-                return filename.substring(filename.lastIndexOf('/') + 1).substring(filename.lastIndexOf('\\') + 1); // MSIE fix.
-            }
-        }
-        return null;
-    }
+	private String getFileTyp() {
+		return file.getContentType().substring(file.getContentType().indexOf("/")+1);
+	}
+	
+//	private static String getFilename(Part part) {
+//        for (String cd : part.getHeader("content-disposition").split(";")) {
+//  
+//        	if (cd.trim().startsWith("filename")) {
+//                String filename = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+//                return filename.substring(filename.lastIndexOf('/') + 1).substring(filename.lastIndexOf('\\') + 1); // MSIE fix.
+//            }
+//        }
+//        return null;
+//    }
 	
 	// Getter / Setter
 
